492121b28fa33aa181ace3ab8d1fd90676e44bc0
poonam <raghuvanshipoonam96@gmail.com>
2023-06-20 14:37:10 +0200

    customer-app basic routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { AddCustomerComponent } from './components/customer/add-customer/add-customer.component';
+import { AddProductsComponent } from './components/products/add-products/add-products.component';
+import { CustomerDetailsComponent } from './components/customer/customer-details/customer-details.component';
+import { ProductDetailsComponent } from './components/products/product-details/product-details.component';
+import { CustomerListComponent } from './components/customer/customer-list/customer-list.component';
+import { ProductListComponent } from './components/product/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomePageComponent},
+  {path:'customers', component:CustomerListComponent},
+  {path:'products', component:ProductListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { AddCustomerComponent } from './components/customer/add-customer/add-customer.component';
+import { CustomerDetailsComponent } from './components/customer/customer-details/customer-details.component';
+import { AddProductsComponent } from './components/products/add-products/add-products.component';
+import { ProductDetailsComponent } from './components/products/product-details/product-details.component';
+import { CustomerListComponent } from './components/customer/customer-list/customer-list.component';
+import { ProductListComponent } from './components/product/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ProductDetailsComponent,
+    AddCustomerComponent,
+    AddProductsComponent,
+    CustomerDetailsComponent,
+    CustomerListComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
